fcbedc7d87bce73a32ab3cece617820c73e8f455
langendorfr <ryan.langendorf@colorado.edu>
2017-07-07 11:42:20 -0600

    Updated gini.R documentation

--- R/gini.R
+++ R/gini.R
@@ -2,7 +2,7 @@
 #'
 #' @description Takes a matrix and returns the Gini coefficient of each column.
 #'
-#' @param input A matrix where the Gini coefficient will be calculated on each column.
+#' @param input A matrix where the Gini coefficient will be calculated on each column. Note that vector data must be converted to a single-column matrix.
 #' 
 #' @param byrow Defaults to FALSE. Set to TRUE to calculate the Gini coefficient of each row.
 #'
@@ -11,7 +11,12 @@
 #' @references Gini, C. (1912). Variabilita e mutabilita. Reprinted in Memorie di metodologica statistica (Ed. Pizetti E, Salvemini, T). Rome: Libreria Eredi Virgilio Veschi.
 #'
 #' @examples
-#' gini(matrix(runif(20, 0, 1), nrow = 10, ncol = 2))
+#' # Vectors are not supported. First convert to a single-column matrix.
+#' sample_data <- runif(20, 0, 1)
+#' gini(matrix(sample_data, ncol = 1))
+#' 
+#' # Multiple Gini coefficients can be calculated simultaneously
+#' gini(matrix(sample_data, ncol = 2)) 
 #' 
 #' @export
 
@@ -28,9 +33,10 @@ gini <- function(input, byrow = FALSE)
   # Convert the input to a column-stochastic matrix (won't affect inputs already in that format)
   matrix <- sweep(input, 2, Matrix::colSums(input), FUN = "/")
 
-  # Sort each column into ascending order (a CDF)
+  # Sort each column into ascending order
   sorted <- apply(matrix, 2, sort)
 
+  # Turn each column into a cumulative distribution function (CDF)
   cdf <- matrix(NA, nrow = rows, ncol = cols)
   cdf[1, ] <- sorted[1, ]
   for (i in 2:rows) {
@@ -41,11 +47,11 @@ gini <- function(input, byrow = FALSE)
   # The Gini coefficient is relative to a perfectly equitable distribution
   equality <- seq(from = 0, to = 1, by = 1/rows)
 
+  # Calculate the Gini coefficient for each column (row if byrow = TRUE)
   output <- rep(NA, cols)
   for (i in 1:cols) {
     output[i] <- 2*(0.5 - pracma::trapz(equality, cdf[, i]))
   }
-  
   
   # The output is a vector of Gini coefficients for each column (row if byrow = TRUE) of the input matrix
   return(output)
