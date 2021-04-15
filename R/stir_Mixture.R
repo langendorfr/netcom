03ebe81d3ad1e7714f57738ce3a3b676a77e1b39
langendorfr <rlangendorf@gmail.com>
2021-04-15 15:57:21 -0600

    Fixed stir_Mixture() so it would correctly stir the number of times specified. Added figures directory, and method figure from alignment paper to start making a badge.

--- R/stir_Mixture.R
+++ R/stir_Mixture.R
@@ -69,27 +69,28 @@ stir_Mixture <- function(matrix, sequence, stirs = 100, p_ER = 0.5, power_PA = 2
     sequence_stir = {}
     for (i in 1:stirs) {
         sequence_stir = c(sequence_stir, sample(x = sequence_id, size = length(sequence_id), replace = FALSE))
-    }
 
-    for (s in seq_along(sequence_stir)) {
-        x = sequence_stir[s]
+        for (s in seq_along(sequence_stir)) {
+            x = sequence_stir[s]
 
-        # print(matrix)
-        # print(sequence[x])
+            # print(matrix)
+            # print(sequence[x])
 
-        if (sequence[x] == "ER") {
-            matrix = stir_ER(matrix = matrix, x = x, p = p_ER)
-        } else if (sequence[x] == "PA") {
-            matrix = stir_PA(matrix = matrix, x = x, power = power_PA)
-        } else if (sequence[x] == "DM") {
-            matrix = stir_DM(matrix = matrix, x = x, divergence = divergence_DM, mutation = mutation_DM, link = link_DM, force_connected = force_connected)
-        } else if (sequence[x] == "NM") {
-            matrix = stir_NM(matrix = matrix, x = x, niches = niches, connectance = connectance_NM, directed = directed)
-        } else if (sequence[x] == "SW") {
-            matrix = stir_SW(matrix = matrix, x = x, rewire = rewire_SW)
-        } else {
-            print("ERROR: Model not specified.")
+            if (sequence[x] == "ER") {
+                matrix = stir_ER(matrix = matrix, x = x, p = p_ER)
+            } else if (sequence[x] == "PA") {
+                matrix = stir_PA(matrix = matrix, x = x, power = power_PA)
+            } else if (sequence[x] == "DM") {
+                matrix = stir_DM(matrix = matrix, x = x, divergence = divergence_DM, mutation = mutation_DM, link = link_DM, force_connected = force_connected)
+            } else if (sequence[x] == "NM") {
+                matrix = stir_NM(matrix = matrix, x = x, niches = niches, connectance = connectance_NM, directed = directed)
+            } else if (sequence[x] == "SW") {
+                matrix = stir_SW(matrix = matrix, x = x, rewire = rewire_SW)
+            } else {
+                print("ERROR: Model not specified.")
+            }
         }
+
     }
 
     return(matrix)
